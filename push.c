8e2a120e35dd334e9075a99277bd8b376a5dc791
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-05-19 00:42:24 +0100

    push function

--- push.c
+++ push.c
@@ -2,48 +2,48 @@
 #include <ctype.h>
 void _push(stack_t **head, unsigned int line_number, char *token)
 {
-	int value;
-	stack_t *new_node;
+    int value;
+    stack_t *new_node;
 
-	if (head == NULL)
-	{
-		fprintf(stderr, "L%u: head is NULL\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    if (head == NULL)
+    {
+        fprintf(stderr, "L%u: head is NULL\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	if (token == NULL)
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    if (token == NULL || *token == '\0' || (*token == '-' && *(token + 1) == '\0') || (*token == '+' && *(token + 1) == '\0'))
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	char *ptr = token;
-	while (*ptr != '\0')
-	{
-		if (!isdigit(*ptr) && *ptr != '-' && *ptr != '+')
-		{
-			fprintf(stderr, "L%u: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-		ptr++;
-	}
+    char *ptr = token;
+    while (*ptr != '\0')
+    {
+        if (!isdigit(*ptr) && *ptr != '-' && *ptr != '+')
+        {
+            fprintf(stderr, "L%u: usage: push integer\n", line_number);
+            exit(EXIT_FAILURE);
+        }
+        ptr++;
+    }
 
-	value = atoi(token);
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+    value = atoi(token);
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	new_node->n = value;
-	new_node->prev = NULL;
-	new_node->next = *head;
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *head;
 
-	if (*head != NULL)
-	{
-		(*head)->prev = new_node;
-	}
+    if (*head != NULL)
+    {
+        (*head)->prev = new_node;
+    }
 
-	*head = new_node;
+    *head = new_node;
 }
