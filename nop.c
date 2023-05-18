837af84b871d361ce58413152f7493a19ff7082a
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-05-18 17:41:06 +0100

    the nop opcode

--- nop.c
+++ nop.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * _nop - Ne rien faire
- * @stack: Double pointeur sur la tête de la pile
- * @line_number: Numéro de la ligne en cours d'exécution
+ * _nop - Do nothing
+ * @stack: Double pointer to the head of the stack
+ * @line_number: Current line number being executed
  */
 
 void _nop(stack_t **stack, unsigned int line_number)
