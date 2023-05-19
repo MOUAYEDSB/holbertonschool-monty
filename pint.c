fa0670fe450e14bef782cf911db343bdd1459d93
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-05-19 12:17:12 +0100

    pint function

--- pint.c
+++ pint.c
@@ -1,21 +1,50 @@
 #include "monty.h"
+#include <ctype.h>
 
-/**
- * _pint - prints the value at the top of the stack
- * @stack: The stack to operate on.
- * @line_number: line number
- * Return: The value at the top of the stack
- */
+void _push(stack_t **head, unsigned int line_number, char *token)
+{
+    int value;
+    stack_t *new_node;
+
+    if (head == NULL)
+    {
+        fprintf(stderr, "L%u: head is NULL\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    if (token == NULL || !isdigit(*token) && *token != '-' && *token != '+')
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    value = atoi(token);
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *head;
+
+    if (*head != NULL)
+    {
+        (*head)->prev = new_node;
+    }
+
+    *head = new_node;
+}
 
 void _pint(stack_t **stack, unsigned int line_number)
 {
-	/* Check if the stack is empty */
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	/* Print the value at the top of the stack */
-	printf("%d\n", (*stack)->n);
+    printf("%d\n", (*stack)->n);
 }
