ce33a377edd9e2f6c138c8774a064456e6361106
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-05-20 00:38:10 +0100

    get function

--- getfunc.c
+++ getfunc.c
@@ -21,7 +21,8 @@ void (*getfunc(char *opcode))(stack_t **stack, unsigned int line_number)
 		{"mul", _mul},
 		{"mod", _mod},
 		{"pstr", _pstr},
-		{"rotl", _rotl}, 
+		{"rotl", _rotl},
+		{"rotr", _rotr},
 		{NULL, NULL}};
 	{
 
