79dd1a5f7ad2ebb5a43a0da528632c3288f75a02
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-05-19 20:21:07 +0100

    about the functions

--- getfunc.c
+++ getfunc.c
@@ -6,25 +6,26 @@
  * @opcode: the opcode to be evaluated
  * Return: pointer to the function that corresponds to the opcode
  */
-
 void (*getfunc(char *opcode))(stack_t **stack, unsigned int line_number)
 {
-	int i = 0;
-	instruction_t op_struct[] = {
-		{"pall", _pall},
-		{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"add", _add},
-		{"nop", _nop},
-		{NULL, NULL}};
+    int i = 0;
+    instruction_t op_struct[] = {
+        {"pall", _pall},
+        {"pint", _pint},
+        {"pop", _pop},
+        {"swap", _swap},
+        {"add", _add},
+        {"nop", _nop},
+        {"sub", _sub},
+        {NULL, NULL}
+    };
 
-	/* Loop through the instruction tab and compare the token to the opcode */
-	/* Check if token matches instruction opcode */
-	for (; op_struct[i].opcode != NULL; i++)
-	{
-		if (strcmp(opcode, op_struct[i].opcode) == 0)
-			return (op_struct[i].f);
-	}
-	return (NULL);
+    /* Loop through the instruction tab and compare the token to the opcode */
+    /* Check if token matches instruction opcode */
+    for (; op_struct[i].opcode != NULL; i++)
+    {
+        if (strcmp(opcode, op_struct[i].opcode) == 0)
+            return (op_struct[i].f);
+    }
+    return (NULL);
 }
