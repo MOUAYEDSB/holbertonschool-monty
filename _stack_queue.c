9fbf85fa045c77a345720ae4853bdf13b78423a7
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-05-20 01:19:49 +0100

    the stack and queue opcode

---
+++ _stack_queue.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * _stack - placeholder function for stack (LIFO) behavior
+ * @stack: double pointer to the stack
+ * @line_number: line number of the opcode
+ */
+void _stack(stack_t **stack, unsigned int line_number)
+{
+    /* Implement stack (LIFO) behavior here */
+    (void)stack;
+    (void)line_number;
+}
+
+/**
+ * _queue - placeholder function for queue (FIFO) behavior
+ * @stack: double pointer to the stack
+ * @line_number: line number of the opcode
+ */
+void _queue(stack_t **stack, unsigned int line_number)
+{
+    /* Implement queue (FIFO) behavior here */
+    (void)stack;
+    (void)line_number;
+}
